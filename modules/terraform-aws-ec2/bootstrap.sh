978f6056563e8975d05ce2e52dafb5b7cc27d525
Andre Faria <andre.faria.24@gmail.com>
2024-08-07 15:49:26 -0400

    remove custom modules

--- modules/terraform-aws-ec2/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install -y apache2
-systemctl start apache2
-systemctl enable apache2
-echo "<html><body><h1>Hello World</h1></body></html>" > /var/www/html/index.html
--- modules/terraform-aws-ec2/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "webserver_public_ip" {
-    value = aws_eip.web_server_eip.public_ip
-}
