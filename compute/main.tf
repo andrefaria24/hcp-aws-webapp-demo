d56488f2695c424785550191d7cacf9d56dfac1c
Andre Faria <andre.faria.24@gmail.com>
2024-08-07 13:10:58 -0400

    added networking remote state data block

--- compute/main.tf
+++ compute/main.tf
@@ -15,6 +15,17 @@ terraform {
     }
 }
 
+data "terraform_remote_state" "networking"{
+    backend = "remote"
+
+    config = {
+        organization = "acfaria-hashicorp"
+        workspaces = {
+            name = "networking"
+        }
+  }
+}
+
 provider "aws" {
     region = var.aws_region
     allowed_account_ids = var.aws_accounts
@@ -24,8 +35,8 @@ resource "aws_instance" "web_server" {
     ami           = var.webserver_ami
     instance_type = var.webserver_instance_type
     key_name      = var.key_name
-    subnet_id = module.aws_vpc.public_subnets[0]
-    vpc_security_group_ids = [aws_security_group.allow_http.id]
+    subnet_id = data.terraform_remote_state.network.outputs.aws_pub_sn_id
+    vpc_security_group_ids = [data.terraform_remote_state.network.outputs.aws_sg_http_id]
     user_data = file("./bootstrap.sh")
 }
 
